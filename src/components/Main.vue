c96bbd48756f06cefa418a40a16e839653917578
zzki <wdftmmas234@gmail.com>
2023-11-11 22:47:43 +0530

    123

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,14 +2,22 @@
   <div>This is Main Page</div>
 </template>
 <script setup>
-const modules = import.meta.glob("./**/*.vue");
-console.log(modules);
-const regex = /"\.\/([^"]+)"/;
-const match = modules["./HelloWorld.vue"].toString().match(regex);
-console.log(match);
-const link = document.createElement("link");
-link.href = "./assets/" + match[1];
-console.log(link.href);
-link.rel = "prefetch";
-document.head.appendChild(link);
+const globModules = import.meta.glob("/src/components/HelloWorld.vue");
+
+
+Object.entries(globModules).forEach(([path, globModule]) => {
+  console.log(path, globModule);
+  globModule().then((mod) => {
+    console.log(path + "模块内容", mod.default);
+  });
+});
+
+// const regex = /"\.\/([^"]+)"/;
+// const match = modules["./HelloWorld.vue"].toString().match(regex);
+// console.log(match);
+// const link = document.createElement("link");
+// link.href = "./assets/" + match[1];
+// console.log(link.href);
+// link.rel = "prefetch";
+// document.head.appendChild(link);
 </script>
